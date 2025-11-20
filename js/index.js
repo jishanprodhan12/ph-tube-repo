d8815b696b2848b695f72795b961bb39b417bcfe
Jishan <mdjishanprodhan132@gamil.com>
2025-11-20 12:10:41 -0600

    fetch all button

--- js/index.js
+++ js/index.js
@@ -1,9 +1,32 @@
 // handle category btn 
-function loadCategory(){
+function loadCategory() {
     fetch(`https://openapi.programming-hero.com/api/phero-tube/categories`)
-    .then(res=> res.json())
-    .then(date=> console.log(date))
-    .catch(err=>console.error(err));
+        .then(res => res.json())
+        .then(date => displayCategory(date.categories))
+        .catch(err => console.error(err));
 }
 
+// demo obj 
+// {category_id: '1003', category: 'Comedy'}
+// category
+// : 
+// "Comedy"
+// category_id
+// : 
+// "1003"
+// [[Prototype]]
+// : 
+// Object
+const displayCategory = (category) => {
+    const categoryBtnContainer = document.getElementById('category-btn-container');
+    category.forEach(item => {
+        
+        const categoryBtn = document.createElement('button');
+        categoryBtn.innerHTML = `
+            
+            <button class="btn ">${item.category}</button>
+            `
+        categoryBtnContainer.appendChild(categoryBtn);
+    })
+}
 loadCategory();
